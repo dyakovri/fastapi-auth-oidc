f66e2fa57a0e5ef8ebf587671273b9f2c35a4df6
Roman Dyakov <roman@dyakov.space>
2025-08-03 11:32:45 +0000

    Add provider properties

--- fastapi_auth_oidc/provider.py
+++ fastapi_auth_oidc/provider.py
@@ -7,7 +7,7 @@ from jose import jwt
 from jose.exceptions import ExpiredSignatureError, JWTClaimsError, JWTError
 from typing_extensions import Annotated
 
-from fastapi_auth_oidc.exceptions import UnauthenticatedException, InvalidCredentialsException
+from fastapi_auth_oidc.exceptions import InvalidCredentialsException, UnauthenticatedException
 from fastapi_auth_oidc.settings import OidcSettingsProvider
 from fastapi_auth_oidc.types import IDToken
 
@@ -29,20 +29,37 @@ class OIDCProvider:
         configuration_uri: str,
         client_id: str,
         jwks_uri: str | None = None,
-        userinfo_uri: str | None = None,
         audience: str | None = None,
         issuer: str | None = None,
         token_type: type[IDToken] = IDToken,
     ):
         self._settings = OidcSettingsProvider(
             configuration_uri=configuration_uri,
-            jwks_uri=jwks_uri,
-            userinfo_uri=userinfo_uri,
             client_id=client_id,
+            jwks_uri=jwks_uri,
             audience=audience,
             issuer=issuer,
         )
         self._token_type = token_type
+
+    configuration_uri = property(lambda self: self._settings.configuration_uri)
+    jwks_uri = property(lambda self: self._settings.jwks_uri)
+    userinfo_endpoint = property(lambda self: self._settings.userinfo_endpoint)
+    authorization_endpoint = property(lambda self: self._settings.authorization_endpoint)
+    token_endpoint = property(lambda self: self._settings.token_endpoint)
+
+    configuration = property(lambda self: self._settings.configuration)
+    jwks = property(lambda self: self._settings.jwks)
+
+    def decode_token(self, token: str):
+        return jwt.decode(
+            token=token,
+            key=self._settings.jwks,
+            algorithms=self._settings.signing_algorithms,
+            audience=self._settings.audience,
+            issuer=self._settings.issuer,
+            options={"verify_at_hash": False},
+        )
 
     def __call__(self, creds: Annotated[HTTPAuthorizationCredentials | None, Depends(security)]):
         logger.debug("OIDCProvider called")
@@ -52,14 +69,7 @@ class OIDCProvider:
             raise InvalidCredentialsException(creds)
 
         try:
-            token = jwt.decode(
-                token=creds.credentials,
-                key=self._settings.jwks,
-                algorithms=self._settings.signing_algorithms,
-                audience=self._settings.audience,
-                issuer=self._settings.issuer,
-                options={"verify_at_hash": False},
-            )
+            token = self.decode_token(creds.credentials)
         except (ExpiredSignatureError, JWTClaimsError, JWTError) as exc:
             raise UnauthenticatedException(exc)
 
--- fastapi_auth_oidc/settings.py
+++ fastapi_auth_oidc/settings.py
@@ -1,7 +1,7 @@
 import logging
+
 import requests
 from cachetools.func import ttl_cache
-
 
 logger = logging.getLogger(__name__)
 
@@ -13,17 +13,53 @@ class OidcSettingsProvider:
         configuration_uri: str,
         client_id: str,
         jwks_uri: str | None = None,
-        userinfo_uri: str | None = None,
         audience: str | None = None,
         issuer: str | None = None,
     ):
-        self.configuration_uri = configuration_uri
-        self.client_id = client_id
-        self.jwks_uri = jwks_uri or self.configuration.get("jwks_uri")
-        self.userinfo_uri = userinfo_uri or self.configuration.get("userinfo_endpoint")
-        self.audience = audience or client_id
-        self.issuer = issuer or self.configuration.get("issuer")
-        self.signing_algorithms = self.configuration.get("id_token_signing_alg_values_supported", ["RS256"])
+        self._configuration_uri = configuration_uri
+        self._client_id = client_id
+        self._jwks_uri = jwks_uri
+        self._audience = audience or client_id
+        self._issuer = issuer
+
+    @property
+    def configuration_uri(self):
+        return self._configuration_uri
+
+    @property
+    def client_id(self):
+        return self._client_id
+
+    @property
+    def jwks_uri(self):
+        jwks_uri = self._jwks_uri or self.configuration.get("jwks_uri")
+        if not jwks_uri:
+            raise ValueError("jwks_uri should be set")
+        return jwks_uri
+
+    @property
+    def userinfo_endpoint(self):
+        return self.configuration.get("userinfo_endpoint")
+
+    @property
+    def authorization_endpoint(self):
+        return self.configuration.get("authorization_endpoint")
+
+    @property
+    def token_endpoint(self):
+        return self.configuration.get("token_endpoint")
+
+    @property
+    def audience(self):
+        return self._audience or self._client_id
+
+    @property
+    def issuer(self):
+        return self._issuer or self.configuration.get("issuer")
+
+    @property
+    def signing_algorithms(self):
+        return self.configuration.get("id_token_signing_alg_values_supported", ["RS256"])
 
     @property
     @ttl_cache(maxsize=1, ttl=600)
@@ -34,23 +70,11 @@ class OidcSettingsProvider:
             raise ValueError("oidc_configuration should be dict")
         return result
 
-    def _get_jwks_uri(self):
-        jwks_uri = self.jwks_uri
-        if not jwks_uri:
-            raise ValueError("jwks_uri should be set")
-        return jwks_uri
-
     @property
     @ttl_cache(maxsize=1, ttl=600)
     def jwks(self):
-        logger.info("Fetching jwks from %s", self._get_jwks_uri())
-        result = requests.get(self._get_jwks_uri()).json()
+        logger.info("Fetching jwks from %s", self.jwks_uri)
+        result = requests.get(self.jwks_uri).json()
         if not isinstance(result, dict):
             raise ValueError("oidc_configuration should be dict")
         return result
-
-    def _get_userinfo_uri(self):
-        userinfo_uri = self.userinfo_uri
-        if not userinfo_uri:
-            raise ValueError("userinfo_uri should be set")
-        return userinfo_uri
